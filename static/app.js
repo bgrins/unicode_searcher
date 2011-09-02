8e2403d0c88dabd7e1b59aff2fbaa12455f42f84
bgrins <briangrinstead@gmail.com>
2011-09-02 13:42:15 -0500

    adding in cheesy colorpicker

--- static/app.js
+++ static/app.js
@@ -1,4 +1,19 @@
+window.log = function(){
+  log.history = log.history || [];   // store logs to an array for reference
+  log.history.push(arguments);
+  if(this.console) {
+    arguments.callee = arguments.callee.caller;
+    var newarr = [].slice.call(arguments);
+    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+  }
+};
 
+// make it safe to use console.log always
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
+
+
+            
 $(function() {
     
     $("#search").append("<input id='partial' name='partial' value='true' type='hidden' />");
@@ -6,4 +21,13 @@ $(function() {
 		target:        '#chars',
     });
     
+colorPicker.exportColor = function() {
+	$(".small-previews2").css("color", "rgb(" + colorPicker.CP.rgbRND + ")");
+}
+
+   /*
+   	$("#color").change(function() {
+   		$(".small-previews2").css("color", $(this).val())
+   	});
+   	*/ 
 });
